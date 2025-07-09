571122d91acc21630cde1d64534440aae6fb063a
Alice Smith <alice@webage.com>
2025-07-09 14:19:10 -0400

    templates for db records

--- backend/generate_data.js
+++ backend/generate_data.js
@@ -1,12 +1,58 @@
+import { MongoClient, ObjectId } from 'mongodb';
 
-const movie_template = {
+const movie_template = { //how to structure a movie for the db
     "_id": {
         "$oid": ""
     },
-    "Title": "",
-    "Director": "",
-    "Release_Date": "",
-    "Genre": "",
-    "Average_Rating": "",
-    "Description": ""
+    "title": "",
+    "director": "",
+    "release_date": "Date object",
+    "genre": [],
+    "average_rating": 0.0,
+    "description": "",
+    "price": 0.0,
+    "img_link": "",
+    "trailer_link": ""
+
+}
+
+const user_template = {
+    "_id": {
+        "$oid": ""
+    },
+    "user_id": "",
+    "password": ""
+}
+
+const cart_template = {
+    "_id": {
+        "$oid": ""
+    },
+    "user_id": "",
+    "credit_card": "",
+    "address": "",
+    "items": ["movie ids"],
+    "ordered": False
+}
+
+try {
+    // TODO: Make a POST request to the API to add the sock
+    const response = await fetch(`${import.meta.env.VITE_SOCKS_API_URL}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(submission),
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    // Handle post submission logic (like showing a success message)
+} catch (error) {
+    console.error("Error posting data", error);
+    // Handle errors here
 }
