39e93dfa7d712b5169e4eb4193067e236dabc507
Alice Smith <alice@webage.com>
2025-07-09 13:47:16 -0400

    added two backend files

---
+++ backend/generate_data.js
@@ -0,0 +1,12 @@
+
+const movie_template = {
+    "_id": {
+        "$oid": ""
+    },
+    "Title": "",
+    "Director": "",
+    "Release_Date": "",
+    "Genre": "",
+    "Average_Rating": "",
+    "Description": ""
+}
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
+
+const app = express();
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
+const PORT = 3000;
+
+// Endpoint to read and send JSON file content
+app.get('/movies', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const movies = await collection.find({}).toArray();
+        res.json(movies);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
+    }
+});
