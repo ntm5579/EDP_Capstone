df23647eb36c1b8c83832d00215fd3b4a34e7174
Alice Smith <alice@webage.com>
2025-07-10 09:49:50 -0400

    checking something

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,83 +13,14 @@ const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 const collectionName = process.env.MONGO_DB_COLLECTION;
 
-
-async function connectToDb() {
-  const client = await MongoClient.connect(url);
-  const db = client.db(dbName);
-  return db;
-}
-
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use("/", movies)
+app.use("/", films)
+app.use("/", directors)
 
-// Sample route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Endpoint to read and send JSON file content
-app.get('/movies', async (req, res) => {
-  try {
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-    const movies = await collection.find({}).toArray();
-    res.json(movies);
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
-  }
-});
-
-app.get('/movies/:id', async (req, res) => {
-  try {
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const { id } = req.params;
-
-    const movie = await collection.find({ _id: new ObjectId(id) }).toArray();
-    res.json(movie);
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
-  }
-});
-
-app.get('/director/:name/movies', async (req, res) => {
-  try {
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const { name } = req.params;
-
-    const movies = await collection.find({ director: name }).toArray();
-    res.json(movies);
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
-  }
-});
-
-app.get('/genre/:genre/movies', async (req, res) => {
-  try {
-    const client = await MongoClient.connect(url);
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const { genre } = req.params;
-
-    const movies = await collection.find({ genre: genre }).toArray();
-    res.json(movies);
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
-  }
-});
 
 // Start server
 app.listen(PORT, () => {
