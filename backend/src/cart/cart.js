cf98d96d2e7f40d3e1bbfe830be2a88c0806b534
Alice Smith <alice@webage.com>
2025-07-11 10:03:43 -0400

    added delete request

--- backend/src/cart/cart.js
+++ backend/src/cart/cart.js
@@ -8,33 +8,8 @@ const cart = express.Router();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
 
-/*
-cart.get('/cart/:id', async (req, res) => {
-    try {
-        const collection = db.collection(collectionName);
-
-        const { name } = req.params;
-
-        const movies = await collection.find({ director: name }).toArray();
-        res.json(movies);
-    } catch (err) {
-        console.error("Error:", err);
-        res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
-    }
-}
-);
-*/
-
 cart.post('/cart/:movie_id', async (req, res) => {
-    /*
-    cart object {
-        _id:ObjectId
-        id: int
-        movies: [movie_ids]
-        last_update: date
-        ordered: boolean
-    }
-    */
+
     try {
         const client = await MongoClient.connect(url);
         const db = client.db(dbName);
@@ -51,7 +26,6 @@ cart.post('/cart/:movie_id', async (req, res) => {
             }
             const result = await collection.insertOne(cart);
             res.status(201).send(`{"_id":"${result.insertedId}"}`);
-            //insert database
         }
         else {
             const cart = carts[0]; //set this to a new cart
@@ -64,6 +38,32 @@ cart.post('/cart/:movie_id', async (req, res) => {
             );
             res.status(201).send(`{"_id":"${cart._id}"}`);
         }
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
+    }
+});
+
+cart.post('/cart/remove/:movie_id', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection("Order");
+
+        const { movie_id } = req.params;
+        console.log(`Adding movie with id ${movie_id}to cart:`);
+        const carts = await collection.find({ 'ordered': false }).toArray();
+
+        const cart = carts[0]; //set this to a new cart
+        cart.movies.pop(cart.movies.indexOf(movie_id));
+        await collection.updateOne(
+            { "_id": cart._id },
+            {
+                $set: { 'last_update': new Date(), movies: cart.movies },
+            }
+        );
+        res.status(201).send(`{"_id":"${cart._id}"}`);
+
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something is wrong... No movies for you! ☹");
